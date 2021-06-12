d3bb2393e49decca2716df1ec933dea6f3770ab8
BalajiDevargudi <85797879+BalajiDevargudi@users.noreply.github.com>
2021-06-12 20:44:12 +0530

    Add files via upload

---
+++ ekart/BrinjalOrder1.js
@@ -0,0 +1,7 @@
+function multipleBy()
+{
+   num1 = document.getElementById("25").value;
+     num2 = document.getElementById("Values").value; 
+
+ document.getElementById("result").BrinjalOrder1.html = num1 * num2;
+   } 
